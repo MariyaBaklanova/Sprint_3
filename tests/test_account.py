458bd338fd456bc32b91e7e50e36d66128375eb0
Mariia Baklanova <pavlovaeshka@yandex.ru>
2024-05-31 10:36:05 +0300

    the test was changed, instead of time.sleep(), it was prescribed WebDriverWait

--- tests/test_account.py
+++ tests/test_account.py
@@ -1,6 +1,7 @@
 from conftest import *
 from locators import Locators
-import time
+from selenium.webdriver.support import expected_conditions as EC
+
 
 class TestAccount:
 
@@ -11,12 +12,12 @@ class TestAccount:
         assert login_h2 == 'Вход'
 
     def test_account_after_login(self, driver):  # по клику на «Личный кабинет» после авторизации
-        driver.implicitly_wait(3)
         driver.find_element(By.XPATH, Locators.AUTHORIZATION_BUTTON).click()
         driver.find_element(By.XPATH, Locators.EMAIL_FIELD).send_keys('marybaklanova006@ya.ru')
         driver.find_element(By.XPATH, Locators.PASSWORD_FIELD).send_keys('123yaya')
         driver.find_element(By.XPATH, Locators.LOGIN_BUTTON).click()
-        time.sleep(1)
-        driver.find_element(By.XPATH, Locators.PROFILE_BUTTON).click()
-        save_button = driver.find_element(By.XPATH, Locators.SAVE_BUTTON).text
+        #time.sleep(1)
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, Locators.PROFILE_BUTTON))).click()
+        #driver.find_element(By.XPATH, Locators.PROFILE_BUTTON).click()
+        save_button = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, Locators.SAVE_BUTTON))).text
         assert save_button == 'Сохранить'

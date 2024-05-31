24e89b76d0109c2ef6cab4124b08cab99a3c88f4
Mariia Baklanova <pavlovaeshka@yandex.ru>
2024-05-31 10:31:00 +0300

    added import expected_conditions as EC

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
-
+from selenium.webdriver.support import expected_conditions as EC
 
 @pytest.fixture(scope='function')
 def driver():

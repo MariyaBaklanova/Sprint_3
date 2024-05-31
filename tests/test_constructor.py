ea1f327ce6ac6c2a6a46e5d5d6b7ecf55c35c27a
Mariia Baklanova <pavlovaeshka@yandex.ru>
2024-05-31 15:05:21 +0300

    the test data of the email and password are placed in the fixture

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -2,35 +2,41 @@ from conftest import *
 from locators import Locators
 
 
+
+
 class TestConstructor:
 
-    def test_buns_section(self, driver):  # переход к разделу «Булки»
+
+    def test_buns_section(self, driver, login_data):  # переход к разделу «Булки»
         driver.implicitly_wait(3)
         driver.find_element(By.XPATH, Locators.AUTHORIZATION_BUTTON).click()
-        driver.find_element(By.XPATH, Locators.EMAIL_FIELD).send_keys('marybaklanova006@ya.ru')
-        driver.find_element(By.XPATH, Locators.PASSWORD_FIELD).send_keys('123yaya')
+        driver.find_element(By.XPATH, Locators.EMAIL_FIELD).send_keys(login_data['email'])
+        driver.find_element(By.XPATH, Locators.PASSWORD_FIELD).send_keys(login_data['password'])
         driver.find_element(By.XPATH, Locators.LOGIN_BUTTON).click()
         driver.find_element(By.XPATH, Locators.SAUCES_SECTION).click()
-        driver.find_element(By.XPATH, Locators.NO_SELECT_BUNS_SECTION)
         driver.find_element(By.XPATH, Locators.BUNS_SECTION).click()
-        driver.find_element(By.XPATH, Locators.SELECT_BUNS_SECTION)
+        assert 'tab_tab_type_current' in driver.find_element(By.XPATH, Locators.BUNS_SECTION).get_attribute('class')
 
 
-    def test_sauces_section(self, driver):  # переход к разделу «Соусы»
+    def test_sauces_section(self, driver, login_data):  # переход к разделу «Соусы»
         driver.implicitly_wait(3)
         driver.find_element(By.XPATH, Locators.AUTHORIZATION_BUTTON).click()
-        driver.find_element(By.XPATH, Locators.EMAIL_FIELD).send_keys('marybaklanova006@ya.ru')
-        driver.find_element(By.XPATH, Locators.PASSWORD_FIELD).send_keys('123yaya')
+        driver.find_element(By.XPATH, Locators.EMAIL_FIELD).send_keys(login_data['email'])
+        driver.find_element(By.XPATH, Locators.PASSWORD_FIELD).send_keys(login_data['password'])
         driver.find_element(By.XPATH, Locators.LOGIN_BUTTON).click()
         driver.find_element(By.XPATH, Locators.SAUCES_SECTION).click()
-        driver.find_element(By.XPATH, Locators.SELECT_SAUCES_SECTION)
+        assert 'tab_tab_type_current' in driver.find_element(By.XPATH, Locators.SAUCES_SECTION).get_attribute(
+            'class')
 
 
-    def test_toppings_section(self, driver):  # переход к разделу «Начинки»
+
+    def test_toppings_section(self, driver, login_data):  # переход к разделу «Начинки»
         driver.implicitly_wait(3)
         driver.find_element(By.XPATH, Locators.AUTHORIZATION_BUTTON).click()
-        driver.find_element(By.XPATH, Locators.EMAIL_FIELD).send_keys('marybaklanova006@ya.ru')
-        driver.find_element(By.XPATH, Locators.PASSWORD_FIELD).send_keys('123yaya')
+        driver.find_element(By.XPATH, Locators.EMAIL_FIELD).send_keys(login_data['email'])
+        driver.find_element(By.XPATH, Locators.PASSWORD_FIELD).send_keys(login_data['password'])
         driver.find_element(By.XPATH, Locators.LOGIN_BUTTON).click()
         driver.find_element(By.XPATH, Locators.TOPPINGS_SECTION).click()
-        driver.find_element(By.XPATH, Locators.SELECT_TOPPINGS_SECTION)
+        assert 'tab_tab_type_current' in driver.find_element(By.XPATH, Locators.TOPPINGS_SECTION).get_attribute(
+            'class')
+

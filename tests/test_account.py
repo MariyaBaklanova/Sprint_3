509483922b35f2df9b0e22aedc471ad6ece74836
Mariia Baklanova <pavlovaeshka@yandex.ru>
2024-05-31 10:51:52 +0300

    changed the names of the tests

--- tests/test_account.py
+++ tests/test_account.py
@@ -4,14 +4,14 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 class TestAccount:
-
-    def test_account_before_login(self, driver):  # по клику на «Личный кабинет» до авторизации
+    #Переход в личный кабинет
+    def test_account_login_before_authorization(self, driver):  # по клику на «Личный кабинет» до авторизации
         driver.implicitly_wait(3)
         driver.find_element(By.XPATH, Locators.PROFILE_BUTTON).click()
         login_h2 = driver.find_element(By.XPATH, Locators.LOGIN_H2).text
         assert login_h2 == 'Вход'
 
-    def test_account_after_login(self, driver):  # по клику на «Личный кабинет» после авторизации
+    def test_account_login_after_authorization(self, driver):  # по клику на «Личный кабинет» после авторизации
         driver.find_element(By.XPATH, Locators.AUTHORIZATION_BUTTON).click()
         driver.find_element(By.XPATH, Locators.EMAIL_FIELD).send_keys('marybaklanova006@ya.ru')
         driver.find_element(By.XPATH, Locators.PASSWORD_FIELD).send_keys('123yaya')

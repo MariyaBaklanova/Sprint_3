0a8722204b9e36d84021d1ff3dfb35e097db7d9f
Mariia Baklanova <pavlovaeshka@yandex.ru>
2024-05-31 15:36:19 +0300

    entering test data now from the fixture

--- tests/test_from_account_to_constructor.py
+++ tests/test_from_account_to_constructor.py
@@ -11,11 +11,11 @@ class TestFromAccount_to_constructor:
         constructor_h1 = driver.find_element(By.XPATH, Locators.CONSTRUCTOR_H1).text
         assert constructor_h1 == 'Соберите бургер'
 
-    def test_move_by_click_to_logo_with_registration(self, driver):  # по клику на логотип Stellar Burgers
+    def test_move_by_click_to_logo_with_registration(self, driver, login_data):  # по клику на логотип Stellar Burgers
         driver.implicitly_wait(3)
         driver.find_element(By.XPATH, Locators.PROFILE_BUTTON).click()
-        driver.find_element(By.XPATH, Locators.EMAIL_FIELD).send_keys('marybaklanova006@ya.ru')
-        driver.find_element(By.XPATH, Locators.PASSWORD_FIELD).send_keys('123yaya')
+        driver.find_element(By.XPATH, Locators.EMAIL_FIELD).send_keys(login_data['email'])
+        driver.find_element(By.XPATH, Locators.PASSWORD_FIELD).send_keys(login_data['password'])
         driver.find_element(By.XPATH, Locators.LOGIN_BUTTON).click()
         driver.find_element(By.XPATH, Locators.LOGO).click()
         constructor_h1 = driver.find_element(By.XPATH, Locators.CONSTRUCTOR_H1).text

4f2386b84a3d50b9667bb9d39bf43688815f4df1
Mariia Baklanova <pavlovaeshka@yandex.ru>
2024-05-31 15:32:01 +0300

    added url

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 import selenium
 import pytest
 from selenium import webdriver
+from urls import URLS
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
@@ -13,7 +14,7 @@ def driver():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--window-size=1920,1080')
     driver = webdriver.Chrome(options=chrome_options)
-    driver.get("https://stellarburgers.nomoreparties.site/")
+    driver.get(URLS.MAIN_PAGE_URL)
     yield driver
     driver.quit()
 
